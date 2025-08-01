103cd82eb32f114c2bb5cbd2100b058c59d46c2f
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-08-01 20:52:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css"
 
 function App() {
     return (
-        <div className="text-3xl font-bold text-green-800">Namaste netflix</div>
+        <div className="text-3xl font-bold text-green-800">Namaste netflix hello</div>
     )
 }
 
